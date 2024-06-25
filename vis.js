02e76daef230c2fad7448facbd6312b3c7082739
adminKing123 <116816518+adminKing123@users.noreply.github.com>
2024-06-25 16:47:00 +0530

    Update vis.js

--- vis.js
+++ vis.js
@@ -1,1 +1,9 @@
 // anaylizer here
+
+import AudioMotionAnalyzer from "https://cdn.skypack.dev/audiomotion-analyzer?min";
+const audioMotion = new AudioMotionAnalyzer(
+  document.getElementById("visualizer-container"),
+  {
+    source: document.getElementById("player"),
+  }
+);
