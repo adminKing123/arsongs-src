483a7423e3f72e59711491f5760f6d8ef1b63318
adminKing123 <116816518+adminKing123@users.noreply.github.com>
2024-08-02 15:23:53 +0530

    Delete vis.js

--- vis.js
+++
@@ -1,9 +0,0 @@
-// anaylizer here
-
-import AudioMotionAnalyzer from "https://cdn.skypack.dev/audiomotion-analyzer?min";
-const audioMotion = new AudioMotionAnalyzer(
-  document.getElementById("visualizer-container"),
-  {
-    source: document.getElementById("player"),
-  }
-);
